7a9276120b982dea295f86cd1de6f49147c09876
alimedon78 <105810030+alimedon78@users.noreply.github.com>
2025-08-05 18:16:30 +0100

    Update bappah high-courts.blade.php

--- resources/views/high-courts.blade.php
+++ resources/views/high-courts.blade.php
@@ -255,7 +255,21 @@
 						</div>
 					</div>
 				</div>
-					<!-- Team Block -->
+					
+				<!-- Team Block -->
+				<div class="team-block col-lg-3 mx-auto col-md-6 col-sm-12">
+					<div class="inner-box wow fadeInLeft" data-wow-delay="450ms" data-wow-duration="1500ms">
+						<div class="image">
+							<img src="assets/images/resource/Hon-j-baffa.jpg" alt="" />
+						</div>
+						<div class="lower-box">
+							<h5><a href="#">Hon. Justice  Hafsat Isah Bappah</a></h5>
+							<div class="designation">Judge, High  Court</div>
+							<a class="arrow flaticon-right-arrow-3" href="#"></a>
+						</div>
+					</div>
+				</div>
+<!-- Team Block -->
 					<div class="team-block col-lg-3 mx-auto col-md-6 col-sm-12">
 						<div class="inner-box wow fadeInLeft" data-wow-delay="300ms" data-wow-duration="1500ms">
 							<div class="image">
@@ -270,20 +284,6 @@
 					</div>
 				
 			
-				<!-- Team Block -->
-				<div class="team-block col-lg-3 mx-auto col-md-6 col-sm-12">
-					<div class="inner-box wow fadeInLeft" data-wow-delay="450ms" data-wow-duration="1500ms">
-						<div class="image">
-							<img src="assets/images/resource/Hon-j-baffa.jpg" alt="" />
-						</div>
-						<div class="lower-box">
-							<h5><a href="#">Hon. Justice  Hafsat Isa Baffa</a></h5>
-							<div class="designation">Judge, High  Court</div>
-							<a class="arrow flaticon-right-arrow-3" href="#"></a>
-						</div>
-					</div>
-				</div>
-
 				</div>
 
 			<div class="row clearfix">
